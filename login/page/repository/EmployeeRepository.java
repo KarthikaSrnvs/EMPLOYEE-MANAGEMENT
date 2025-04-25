654954bbe4912c6ec1e31ebcf796e10d572d4191
KarthikaSrnvs <128312503+KarthikaSrnvs@users.noreply.github.com>
2025-04-25 13:57:02 +0530

    Delete login/page directory

--- login/page/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package login.page.repository;
-
-import login.page.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    
-}
